2aa82ec7a4d6a85da3709a5b1bbdba4c2e4deb7f
jingjitree <jingjitree@gmail.com>
2020-12-09 18:21:56 +0800

    常量类构造函数私有化

--- src/main/java/top/inson/springboot/constants/RedisConstants.java
+++ src/main/java/top/inson/springboot/constants/RedisConstants.java
@@ -2,10 +2,11 @@ package top.inson.springboot.constants;
 
 public class RedisConstants {
 
+    private RedisConstants(){}
+
     public static final String GOODS_STOCK_KEY = "goods_stock";
 
     public static final String GOODS_STOCK_LOCK_KEY = "goods_stock_lock";
-
 
 
 }
